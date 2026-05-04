58712b48857035081430c9923d1a523e1fec625c
Luciano () <luciano1090@gmail.com>
2026-05-04 20:31:30 -0300

    feat: initialize server and integrate nodemailer for email functionality

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./src/config/db.js";
 import productRoutes from "./src/routes/productRoutes.js";
+import { sendEmail } from "./src/utils/nodemailer.js";
 
 dotenv.config();
 
@@ -21,6 +22,17 @@ app.get("/", (req, res) => {
 
 // routes
 app.use("/api/products", productRoutes);
+
+// --- RUTA TEMPORAL DE PRUEBA NODEMAILER ---
+app.get("/test-email", async (req, res) => {
+  try {
+    await sendEmail(process.env.EMAIL_ADDRESS, "Prueba desde Node", "¡Funciona perfecto!");
+    res.send("¡Correo de prueba enviado! Revisá tu bandeja en Mailtrap.");
+  } catch (error) {
+    res.status(500).send("Hubo un error al enviar el correo.");
+  }
+});
+// ------------------------------------------
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
